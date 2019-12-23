f0dd02033dca302ef6a272b343e5e5ce1f23c265
Siddartha <siddartha.bommireddy@digital.homeoffice.gov.uk>
2019-12-23 22:46:33 +0000

    Modified Dockerfile and shellscript

--- app/bin/run_nats.sh
+++ app/bin/run_nats.sh
@@ -3,8 +3,11 @@ alias echo='echo $(date)'
 echo started
 ts=$(date +%Y%m%d%H%M%S)
 
-python3 /NATS/scripts/prepare_batch_csv.py > /NATS/log/prepare_batch${ts}_$$_${RANDOM}_$(hostname).txt 2> /NATS/log/prepare_batch${ts}_$$_${RANDOM}_$(hostname).log
-python3 /NATS/scripts/download_batchfiles.py > /NATS/log/download${ts}_$$_${RANDOM}_$(hostname).txt 2> /NATS/log/download${ts}_$$_${RANDOM}_$(hostname).log &
-python3 /NATS/scripts/upload_batchfiles.py.py
+ver=$(python --version)
+echo $ver
+
+python /NATS/scripts/prepare_batch_csv.py > /NATS/log/prepare_batch${ts}_$$_${RANDOM}_$(hostname).txt 2> /NATS/log/prepare_batch${ts}_$$_${RANDOM}_$(hostname).log
+python /NATS/scripts/download_batchfiles.py > /NATS/log/download${ts}_$$_${RANDOM}_$(hostname).txt 2> /NATS/log/download${ts}_$$_${RANDOM}_$(hostname).log &
+python /NATS/scripts/upload_batchfiles.py
 
 echo Ended
