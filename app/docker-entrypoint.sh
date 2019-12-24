58423c0ce97e3b6b097be5c097a0706f4502275e
David Takacs <info@wirelab.co.uk>
2019-12-24 12:22:45 +0000

    update variables and folder structure

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 # Copy all files and folders from the staging area to PVC
-cp -R /tmp/NATS/data /NATS
 cp -R /tmp/NATS/stage /NATS
 cp -R /tmp/NATS/scripts /NATS
-cp -R /tmp/NATS/quarantine /NATS
 cp -R /tmp/NATS/log /NATS
 cp -R /tmp/NATS/bin /NATS
-cp -R /tmp/NATS/failed_to_parse /NATS
 
 # Set permissions
-chown -R runner:runner /NATS/data
 chown -R runner:runner /NATS/stage
 chown -R runner:runner /NATS/scripts
-chown -R runner:runner /NATS/quarantine
 chown -R runner:runner /NATS/log
 chown -R runner:runner /NATS/bin
-chown -R runner:runner /NATS/failed_to_parse
 
 exec "$@"
